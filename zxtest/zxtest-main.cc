74d5aa29f0ee5b193d28c160c95e6cb22fd8173c
Peng He <xnhp0320@gmail.com>
2024-06-01 16:22:41 +0800

    add zxtest for fbl testing.
    slowly working to make everything work in non-Zircon.

---
+++ zxtest/zxtest-main.cc
@@ -0,0 +1,17 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+#include <zircon/compiler.h>
+
+#include <zxtest/zxtest.h>
+
+// Provides a default entry point to run all registered tests. If the main program provides its own
+// main, the library's main will be ignored.
+__WEAK int main(int argc, char** argv) {
+  // TODO(https://fxbug.dev/42073486): Remove this once the elf runner no longer
+  // fools libc into block-buffering stdout.
+  setlinebuf(stdout);
+  return RUN_ALL_TESTS(argc, argv);
+}
